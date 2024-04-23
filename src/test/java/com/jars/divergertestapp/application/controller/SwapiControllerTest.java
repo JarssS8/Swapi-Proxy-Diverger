ddc062081cac5c67df0bb3dd70184e97c6be51c0
JarssS8 <adriancgs@gmail.com>
2024-04-23 05:45:39 +0200

    Small changes about unused imports

--- src/test/java/com/jars/divergertestapp/application/controller/SwapiControllerTest.java
+++ src/test/java/com/jars/divergertestapp/application/controller/SwapiControllerTest.java
@@ -1,11 +1,9 @@
 package com.jars.divergertestapp.application.controller;
 
-import com.jars.divergertestapp.application.service.SwapiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
