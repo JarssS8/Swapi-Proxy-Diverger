0bb879cb2743bba310c85ec601d4a57f65a6524e
JarssS8 <adriancgs@gmail.com>
2024-04-23 05:07:37 +0200

    Include all models

--- src/main/java/com/jars/divergertestapp/domain/model/Film.java
+++ src/main/java/com/jars/divergertestapp/domain/model/Film.java
@@ -1,13 +1,20 @@
 package com.jars.divergertestapp.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-
-import java.util.Date;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Film {
+    @JsonProperty("title")
     private String name;
-    private Date relaseDate;
+    @JsonProperty("release_date")
+    private String releaseDate;
 }
--- src/main/java/com/jars/divergertestapp/domain/model/VehicleBase.java
+++ src/main/java/com/jars/divergertestapp/domain/model/VehicleBase.java
@@ -1,7 +1,8 @@
 package com.jars.divergertestapp.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -10,7 +11,9 @@ import lombok.experimental.SuperBuilder;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class VehicleBase {
     private String name;
+    @JsonProperty("max_atmosphering_speed")
     private Integer maxAtmosphericSpeed;
 }
